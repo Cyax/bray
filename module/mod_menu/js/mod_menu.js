d489241c1ac5b54fcdca6c07f1ea11b0423e9ac2
Moulin <Moulin@Moulin-PC.home>
2015-04-13 18:25:08 +0200

    Fifth commit

--- module/mod_menu/js/mod_menu.js
+++ module/mod_menu/js/mod_menu.js
@@ -1,34 +1,58 @@
 /**
- * 
+ * Script for module Menu
  */
 
 $(document).ready(function() {
+	
+	$("ul.subMenu").hide();
+	
+	mod_menu_autoset_subMenu();
+	
 	$.each($("ul.menu>li"), function(i, val) {
 		$(val).click(function() {
+			$("ul.subMenu").hide();
 			menuClicked = $(val).find("input[name=menu]").val();
 			console.log(menuClicked + " clicked");
 			
-			$("div#content").children().remove();
-			
-			$.ajax({
-				url: $("input[name='path']").val() + $("input[name='modulePath']").val() + $(val).find("input[name=menu]").val() + "/" + $(val).find("input[name=menu]").val() + ".php",
-			})
-			.done(function(data) {
-				if ( console && console.log ) {
-					$("div#content").append(data);
-				}
-			})
-			.fail(function() {
-					$.ajax({
-						url: $("input[name='path']").val() + $("input[name='modulePath']").val() + "mod_menu/404.php",
-					})
-					.done(function(data) {
-						if ( console && console.log ) {
-							$("div#content").append(data);
-						}
-					})
+			if($("ul." + menuClicked).length>0) {
+				$("ul.subMenu").hide();
+				$("ul." + menuClicked).show();
+			} else {
+				$("div#content").children().remove();
 				
-			});
+				$.ajax({
+					url: $("input[name='path']").val() + $("input[name='modulePath']").val() + $(val).find("input[name=menu]").val() + "/" + $(val).find("input[name=menu]").val() + ".php",
+				})
+				.done(function(data){mod_menu_ajax_done(data);})
+				.fail(function(){mod_menu_ajax_fail();});
+			}
 		})
 	});
 });
+
+function mod_menu_autoset_subMenu() {
+	$.each($("ul.subMenu"), function(i, val) {
+		var className = $("ul.subMenu").attr('class').replace("subMenu ",""); 
+		
+		$("ul." + className).css("left", $("input[value='" + className + "']").parent().offset().left);
+		$("ul." + className).css("top", $("input[value='" + className + "']").parent().offset().top+4);
+	});
+}
+
+function mod_menu_ajax_done(data) {
+	if ( console && console.log ) {
+		$("div#content").append(data);
+	}
+}
+
+function mod_menu_ajax_fail() {
+	$.ajax({
+		url: $("input[name='path']").val() + $("input[name='modulePath']").val() + "mod_menu/404.php",
+	})
+	.done(function(data) {
+		if ( console && console.log ) {
+			$("div#content").append(data);
+		}
+	})
+}
+
--- module/mod_menu/mod_menu.php
+++ module/mod_menu/mod_menu.php
@@ -1,34 +1,36 @@
 <?php
+	$mod_menu_array = parse_ini_file("menuConf.ini", true);
+	$mod_submenu_array = parse_ini_file("subMenuConf.ini", true);
 	echo("<script>cssLoad('module/mod_menu/css/mod_menu.css');</script>");
 	echo("<script>$.getScript('module/mod_menu/js/mod_menu.js');</script>");
 ?>
 				<input type="hidden" name="path" value="<?php echo($ini_array['path']); ?>" />
 				<input type="hidden" name="componentPath" value="<?php echo($ini_array['componentPath']); ?>" />
 				<input type="hidden" name="modulePath" value="<?php echo($ini_array['modulePath']); ?>" />
-
 				<ul class="menu">
-					<li>
-						<input type="hidden" name="menu" value="welcome" />
-						Accueil
-					</li>
-					<li>
-						<input type="hidden" name="menu" value="mod_customer" />
-						Client
-					</li>
-					<li>
-						<input type="hidden" name="menu" value="calendar" />
-						Calendrier
-					</li>
-					<li>
-						<input type="hidden" name="menu" value="statistic" />
-						Statistique
-					</li>
-					<li>
-						<input type="hidden" name="menu" value="alert" />
-						Alerte ()
-					</li>
-					<li>
-						<input type="hidden" name="menu" value="configuration" />
-						Configuration
-					</li>
+<?php 
+	foreach ($mod_menu_array as $key => $value) {
+		echo("<li>");
+		echo($key);
+		if(array_key_exists("sys_value", $value)) {
+			echo("<input type='hidden' name='menu' value='" . $value['sys_value'] . "' />");
+		} else {
+			echo("<input type='hidden' name='menu' value='' />");
+		}
+		echo("</li>");
+	}	
+?>
 				</ul>
+<?php
+foreach ($mod_submenu_array as $key => $value) {
+	foreach ($mod_menu_array as $mykey => $myvalue) {
+		if($key == $mykey) {
+			echo("<ul class='subMenu " . $myvalue['sys_value'] . "'>");
+			foreach ($value as $v) {
+				echo("<li>" . $v . "</li>");
+			}
+			echo("</ul>");
+		}
+	}
+}
+?>
