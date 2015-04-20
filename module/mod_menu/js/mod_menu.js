29602409d52b8f660c4c30e0e85e9c78b29dffb1
Moulin <Moulin@192.168.1.10>
2015-04-20 17:51:14 +0200

    Many fix on Module menu

--- module/mod_menu/js/mod_menu.js
+++ module/mod_menu/js/mod_menu.js
@@ -6,8 +6,6 @@ $(document).ready(function() {
 	
 	$("ul.subMenu").hide();
 	
-	mod_menu_autoset_subMenu();
-	
 	$.each($("ul.menu>li"), function(i, val) {
 		$(val).click(function() {
 			$("ul.subMenu").hide();
@@ -16,11 +14,12 @@ $(document).ready(function() {
 			
 			if($("ul." + menuClicked).length>0) {
 				$("ul.subMenu").hide();
-				$("ul." + menuClicked).show();
+				mod_menu_show_subMenu(menuClicked);
 			} else {
 				$("div#content").children().remove();
 				
 				$.ajax({
+					method: "POST",
 					url: $("input[name='path']").val() + $("input[name='modulePath']").val() + $(val).find("input[name=menu]").val() + "/" + $(val).find("input[name=menu]").val() + ".php",
 				})
 				.done(function(data){mod_menu_ajax_done(data);})
@@ -30,17 +29,32 @@ $(document).ready(function() {
 	});
 });
 
-function mod_menu_autoset_subMenu() {
-	$.each($("ul.subMenu"), function(i, val) {
-		var className = $("ul.subMenu").attr('class').replace("subMenu ",""); 
-		
-		$("ul." + className).css("left", $("input[value='" + className + "']").parent().offset().left);
-		$("ul." + className).css("top", $("input[value='" + className + "']").parent().offset().top+4);
+function mod_menu_show_subMenu(elt) {
+	$("ul." + menuClicked).show();
+	$("ul." + elt).css("left", $("input[value='" + elt + "']").parent().offset().left);
+	$("ul." + elt).css("top", $("input[value='" + elt + "']").parent().offset().top + 5);
+	
+	$.each($("ul." + elt + ">li"), function(i, val) {
+		$(val).click(function() {
+			menuClicked = $(val).find("input[name=menu]").val();
+			console.log(menuClicked + " clicked");
+			$("ul.subMenu").hide();
+			$("div#content").children().remove();
+			
+			$.ajax({
+				method: "POST",
+				url: $("input[name='path']").val() + $("input[name='modulePath']").val() + $(val).find("input[name=menu]").val() + "/" + $(val).find("input[name=menu]").val() + ".php",
+				data: { param: ""+$(val).find("input[name=paramMenu]").val()+""}
+			})
+			.done(function(data){mod_menu_ajax_done(data);})
+			.fail(function(){mod_menu_ajax_fail();});
+		});
 	});
 }
 
 function mod_menu_ajax_done(data) {
 	if ( console && console.log ) {
+		$("div#content").children().remove();
 		$("div#content").append(data);
 	}
 }
--- module/mod_menu/mod_menu.php
+++ module/mod_menu/mod_menu.php
@@ -1,6 +1,5 @@
 <?php
 	$mod_menu_array = parse_ini_file("menuConf.ini", true);
-	$mod_submenu_array = parse_ini_file("subMenuConf.ini", true);
 	echo("<script>cssLoad('module/mod_menu/css/mod_menu.css');</script>");
 	echo("<script>$.getScript('module/mod_menu/js/mod_menu.js');</script>");
 ?>
@@ -8,7 +7,7 @@
 				<input type="hidden" name="componentPath" value="<?php echo($ini_array['componentPath']); ?>" />
 				<input type="hidden" name="modulePath" value="<?php echo($ini_array['modulePath']); ?>" />
 				<ul class="menu">
-<?php 
+<?php
 	foreach ($mod_menu_array as $key => $value) {
 		echo("<li>");
 		echo($key);
@@ -22,15 +21,25 @@
 ?>
 				</ul>
 <?php
-foreach ($mod_submenu_array as $key => $value) {
-	foreach ($mod_menu_array as $mykey => $myvalue) {
-		if($key == $mykey) {
-			echo("<ul class='subMenu " . $myvalue['sys_value'] . "'>");
-			foreach ($value as $v) {
-				echo("<li>" . $v . "</li>");
+	foreach ($mod_menu_array as $key => $value) {
+		if(array_key_exists("sys_value", $value) &&
+				array_key_exists("item_0_sys_value", $value) &&
+				array_key_exists("item_0_name", $value)) {
+			echo("<ul class='subMenu " . $value['sys_value'] . "'>");
+			
+			$i = 0;
+			while(array_key_exists("item_" . $i . "_sys_value", $value) &&
+					array_key_exists("item_" . $i . "_name", $value)) {
+				echo("<li>");
+				echo($value["item_" . $i . "_name"]);
+				echo("<input type='hidden' name='menu' value='" . $value['sys_value'] . "' />");
+				echo("<input type='hidden' name='paramMenu' value='" . $value["item_" . $i . "_sys_value"] . "' />");
+				echo("</li>");
+				
+				$i++;
 			}
+			
 			echo("</ul>");
 		}
 	}
-}
 ?>
